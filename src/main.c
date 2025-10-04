bf2b05b52e6f7a8183631857c2067889efacb0ac
dnordyke2-cmyk <dnordyke2@gmail.com>
2025-10-04 11:52:09 -0400

    Update main.c002

--- src/main.c
+++ src/main.c
@@ -1,13 +1,13 @@
 #include <libdragon.h>
 
 int main(void) {
-    display_init( RESOLUTION_320x240, DEPTH_16_BPP, 2, GAMMA_NONE, ANTIALIAS_OFF );
+    display_init(RESOLUTION_320x240, DEPTH_16_BPP, 2, GAMMA_NONE, ANTIALIAS_OFF);
     rdpq_init();
 
     while (1) {
         surface_t *disp = display_get();
         rdpq_attach(disp);
-        rdpq_clear(0x000000FF);  // black screen
+        rdpq_clear(0x101018FF); // dark gray screen
         rdpq_detach_show();
     }
     return 0;
