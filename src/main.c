fb3206b55c7c2ef70a7b8faee320348b78948ecf
dnordyke2-cmyk <dnordyke2@gmail.com>
2025-10-05 10:08:01 -0400

    Update main.c003

--- src/main.c
+++ src/main.c
@@ -1,13 +1,24 @@
 #include <libdragon.h>
+#include "hud.h"
 
 int main(void) {
+    // Init display + console + RDP
     display_init(RESOLUTION_320x240, DEPTH_16_BPP, 2, GAMMA_NONE, ANTIALIAS_OFF);
     rdpq_init();
+    console_init();
+
+    hud_init();
 
     while (1) {
         surface_t *disp = display_get();
         rdpq_attach(disp);
-        rdpq_clear(0x101018FF); // dark gray screen
+
+        // Background
+        rdpq_clear(RGBA32(32,32,32,255));
+
+        // HUD overlay
+        hud_draw();
+
         rdpq_detach_show();
     }
     return 0;
