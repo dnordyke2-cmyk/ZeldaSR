d2b5c2f6e2d178257154b3cf3aa53f7cd25809d0
dnordyke2-cmyk <dnordyke2@gmail.com>
2025-10-16 09:50:03 -0400

    Update hud.h003

--- src/hud.h
+++ src/hud.h
@@ -1,6 +1,12 @@
-#pragma once
+#ifndef HUD_H
+#define HUD_H
 
 #include <libdragon.h>
 
+/* Initialize HUD resources (fonts, state, etc). */
 void hud_init(void);
-void hud_draw(void);
+
+/* Draw the HUD onto the given framebuffer surface. */
+void hud_draw(surface_t *fb);
+
+#endif /* HUD_H */
