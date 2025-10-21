d72a2afa7bf5f3f4dd19973561c91de121465e4c
dnordyke2-cmyk <dnordyke2@gmail.com>
2025-10-21 16:18:49 -0400

    Update main.c028

--- src/main.c
+++ src/main.c
@@ -1,15 +1,19 @@
 #include <libdragon.h>
 
 int main(void) {
-    dfs_init(DFS_DEFAULT_LOCATION);
-    display_init(RESOLUTION_320x240, DEPTH_16_BPP, 2, GAMMA_NONE, ANTIALIAS_RESAMPLE);
-    rdpq_init();
-    rdpq_set_mode_standard();
+    console_init();
+    console_set_debug(true);
 
-    for (;;) {
-        surface_t *s = display_get();
-        rdpq_attach(s, NULL);
-        rdpq_clear(RGBA32(0, 16, 48, 255));  // dark blue
+    display_init( RESOLUTION_320x240, DEPTH_16_BPP, 2, GAMMA_NONE, ANTIALIAS_OFF );
+    rdpq_init();
+    controller_init();
+
+    console_printf("Shattered Realms booted!\\n");
+    while (1) {
+        while (display_is_next_draw_buffer_busy());
+        surface_t *disp = display_get();
+        rdpq_attach(disp, NULL);
+        rdpq_set_mode_copy( true );
         rdpq_detach_show();
     }
     return 0;
